2c98fbbf8714b298be8fa39826f198274e333381
christianosilvaf <christianosilvaf@gmail.com>
2023-08-29 20:23:35 -0500

    arreglando nombre home

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import './App.css'
 
 import PokemonDetail from './pages/PokemonDetail'
 import Page404 from './pages/Page404'
-import Home from './pages/home'
+import Home from './pages/Home.jsx'
 import Pokedex from './pages/Pokedex'
 import PrivateRoutes from './components/auth/PrivateRoutes'
 
