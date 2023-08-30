233e7bcef498ee47beb1943ef96876db80a66bc3
christianosilvaf <christianosilvaf@gmail.com>
2023-08-30 18:02:07 -0500

    full estilizado, falta trainer en la store y movements

--- src/components/Pokedex/PokemonCard.jsx
+++ src/components/Pokedex/PokemonCard.jsx
@@ -11,26 +11,26 @@ const PokemonCards = ({pokemonURL}) => {
     }
 
     const bgStylePokemon ={
-    normal: 'bg-gradient-to-b from-[#CB98A7] to-[#CB98A7]/30',
-    fighting: 'bg-gradient-to-b from-[#EF6139] to-[#EF6139]/30',
-    flying: 'bg-gradient-to-b from-[#93B2C7] to-[#93B2C7]/30',
-    poison: 'bg-gradient-to-b from-[#9B69DA] to-[#9B69DA]/30',
-    ground: 'bg-gradient-to-b from-[#6E491F] to-[#6E491F]/30',
-    rock: 'bg-gradient-to-b from-[#8B3E22] to-[#8B3E22]/30',
-    bug: 'bg-gradient-to-b from-[#3B9852] to-[#3B9852]/30',
-    ghost: 'bg-gradient-to-b from-[#906791] to-[#906791]/30', 
-    steel: 'bg-gradient-to-b from-[#43BD94] to-[#43BD94]/30',
-    fire: 'bg-gradient-to-b from-[#FD4D5A] to-[#FD4D5A]/30',
-    water: 'bg-gradient-to-b from-[#85A8FC] to-[#85A8FC]/30',
-    grass: 'bg-gradient-to-b from-[#26CC50] to-[#26CC50]/30',
-    electric: 'bg-gradient-to-b from-[#FAFB71] to-[#FAFB71]/30',
-    psychic: 'bg-gradient-to-b from-[#F71C90] to-[#F71C90]/30',
-    ice: 'bg-gradient-to-b from-[#D6F1FA] to-[#D6F1FA]/30',
-    dragon: 'bg-gradient-to-b from-[#60CCD9] to-[#60CCD9]/30',
-    dark: 'bg-gradient-to-b from-[#5A5979] to-[#5A5979]/30',
-    fairy: 'bg-gradient-to-b from-[#EB1269] to-[#EB1269]/30',
-    unknown: 'bg-gradient-to-b from-[#35433E] to-[#35433E]/30',
-    shadow: 'bg-gradient-to-b from-[#301645] to-[#301645]/30',
+    normal: 'bg-gradient-to-b from-[#CB98A7] via-[#CB98A7]/20 to-[#CB98A7]',
+    fighting: 'bg-gradient-to-b from-[#EF6139] via-[#EF6139]/20 to-[#EF6139]',
+    flying: 'bg-gradient-to-b from-[#93B2C7] via-[#93B2C7]/20 to-[#93B2C7]',
+    poison: 'bg-gradient-to-b from-[#9B69DA] via-[#9B69DA]/20 to-[#9B69DA]',
+    ground: 'bg-gradient-to-b from-[#6E491F] via-[#6E491F]/20 to-[#6E491F]',
+    rock: 'bg-gradient-to-b from-[#8B3E22] via-[#8B3E22]/20 to-[#8B3E22]/',
+    bug: 'bg-gradient-to-b from-[#3B9852] via-[#3B9852]/20 to-[#3B9852]',
+    ghost: 'bg-gradient-to-b from-[#906791] via-[#906791]/20 to-[#906791]', 
+    steel: 'bg-gradient-to-b from-[#43BD94] via-[#43BD94]/20 to-[#43BD94]',
+    fire: 'bg-gradient-to-b from-[#FD4D5A] via-[#FD4D5A]/20 to-[#FD4D5A]',
+    water: 'bg-gradient-to-b from-[#85A8FC] via-[#85A8FC]/20 to-[#85A8FC]',
+    grass: 'bg-gradient-to-b from-[#26CC50] via-[#26CC50]/20 to-[#26CC50]',
+    electric: 'bg-gradient-to-b from-[#FAFB71] via-[#FAFB71]/20  to-[#FAFB71]',
+    psychic: 'bg-gradient-to-b from-[#F71C90] via-[#F71C90]/20 to-[#F71C90]',
+    ice: 'bg-gradient-to-b from-[#D6F1FA] via-[#D6F1FA]/20 to-[#D6F1FA]',
+    dragon: 'bg-gradient-to-b from-[#60CCD9] via-[#60CCD9]/20 to-[#60CCD9]',
+    dark: 'bg-gradient-to-b from-[#5A5979] via-[#5A5979]/20 to-[#5A5979]',
+    fairy: 'bg-gradient-to-b from-[#EB1269] via-[#EB1269]/20 to-[#EB1269]',
+    unknown: 'bg-gradient-to-b from-[#35433E] via-[#35433E]/20 to-[#35433E]',
+    shadow: 'bg-gradient-to-b from-[#301645] via-[#301645]/20 to-[#301645]',
 };
 
 
@@ -86,25 +86,46 @@ const textStylePokemon ={
             .catch((error)=> console.log(error));
     },[])
 
+    //${borderStylePokemon[pokemoninfo?.types[0]]}
+    //
     return (
     <Link to={`/Pokedex/${pokemoninfo?.id}`} >
-        <article className={`${borderStylePokemon[pokemoninfo?.types[0]]} text-center rounded-3xl`}>
-            <header className={`h-[80px] ${bgStylePokemon[pokemoninfo?.types[0]]} relative  rounded-t-2xl`} >
-                <div className='absolute left-1/2 -translate-x-1/2 -bottom-4
-                                h-[65px] aspect-square'>
-                    <img className='h-full w-full object-contain' src={pokemoninfo?.image} alt="" />
+        <article className={`border-[5px] border-black text-center rounded-3xl z-1`}>
+            <header className={`flex flex-col items-center justify-center h-[80px] bg-red-700 relative border-b-[5px] border-black rounded-t-2xl z-2`} >
+                <div className={`h-[70px] bg-white rounded-full aspect-square top-10 z-3 
+                        absolute
+                        border-[8px] border-black
+                        after:block after:content-[""] 
+                        after:h-[38px] after:aspect-square
+                        after:bg-red-500  after:rounded-full
+                        after:border-[6px] after:border-black
+                        after:top-[8px] after:right-[8px] after:absolute`}>
                 </div>
             </header>
 
-            <section className='bg-white/90 pt-5 rounded-b-[19px]'>
-                <h3 className='text-lg font-bold capitalize'>{pokemoninfo?.name}</h3>
-                <h4 className='text-sm'>{pokemonTypesFormat(pokemoninfo?.types)}</h4>
-                <span className='font-semibold'>Stats</span>
-                <div>
+            <section className='flex flex-col bg-white/90  rounded-b-[19px] text-center relative z-1 items-center justify-center'>
+                <div className={`h-[70px] bg-white rounded-full aspect-square top-40   
+                        -translate-y-12 absolute
+                        border-[8px] border-black
+                        after:block after:content-[""] 
+                        after:h-[38px] after:aspect-square
+                        after:bg-white  after:rounded-full
+                        after:border-[6px] after:border-black
+                        after:top-[8px] after:right-[8px] after:absolute after:z-1`}>
+                </div>
+                <div className={`flex flex-col w-[100%] ${bgStylePokemon[pokemoninfo?.types[0]]} items-center aboslute z-2`}>
+                    <div className='h-[75px] aspect-square'>
+                        <img className='h-full w-full object-contain' src={pokemoninfo?.image} alt="" />
+                    </div>
+                    <h3 className='text-lg font-bold capitalize'>{pokemoninfo?.name}</h3>
+                    <h4 className='text-sm'>{pokemonTypesFormat(pokemoninfo?.types)}</h4>
+                    <span className='font-semibold'>Stats</span>
+                </div>
+                <div className='border-t-[5px] border-black'>
                     <ul className='grid gap-2 grid-cols-3 text-xs p-2 '>
                         { 
                             pokemoninfo?.stats.map((stat)=>
-                            <li className={`flex flex-col border-[1px] rounded-full ${borderStylePokemon[pokemoninfo?.types[0]]} p-2 gap-1 justify-between`} key={stat.name}> 
+                            <li className={`flex flex-col border-[1px] rounded-full border-gray-300 h-[80px]  p-3 gap-0 justify-center`} key={stat.name}> 
                                 <span className={`capitalize text-[12px] text-center whitespace-pre-wrap text-black/80`} >{stat.name}</span> 
                                 <span className={`font-bold text-[15px] ${textStylePokemon[pokemoninfo?.types[0]]}`}>{stat.value}</span>
                             </li> )
--- src/components/pokemonDetail/StatBarList.jsx
+++ src/components/pokemonDetail/StatBarList.jsx
@@ -8,10 +8,10 @@ const StatBarList = ({stats}) => {
     }
     
     return (
-    <section className='w-[120%] h-[80%] text-[9px] p-2 m-auto'>
-        <h2 className='text-center capitalize'>Stats</h2>
+    <section className='w-[100%] text-[12px] text-sky-950 p-2 m-auto'>
+        <h2 className='text-center capitalize font-semibold text-[20px]'>Stats</h2>
         <article>
-            <ul>
+            <ul className='flex flex-col gap-2 text-[15px] font-bold capitalize'>
                 {
                     stats?.map((stat)=> (
                     <li key={stat.name} className='flex flex-col gap-1'> 
@@ -20,8 +20,10 @@ const StatBarList = ({stats}) => {
                             <span>{stat.value}/255</span>
                         </div>
 
-                        <div className='h-[5px] bg-slate-200 rounded-md'>
-                            <div style={{width: per(stat.value)}} className='h-full  bg-gradient-to-r from-yellow-500 to bg-orange-500'></div>
+                        <div className='h-[15px] bg-slate-200 rounded-md'>
+                            <div style={{width: per(stat.value)}} 
+                                className='h-full  bg-gradient-to-r from-yellow-500 to bg-orange-500
+                                            rounded-l-md'></div>
                         </div>
                     </li>))
                 }
